981db949124b87adeb7d31a7838293a9579c4d4d
SYFR <SYFR@DESKTOP-O7NH43N>
2021-04-26 13:09:17 +0530

    This is the initial code

---
+++ Rehab/src/main/java/com/rb/controller/ThearapistOperationController.java
@@ -0,0 +1,34 @@
+package com.rb.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rb.entity.Thrapist;
+
+@RequestMapping("/pt")
+public class ThearapistOperationController {
+
+	 
+	 public ResponseEntity<Map<String,Object>>saveThreapist(@RequestBody  Thrapist thrapist){
+		 
+        		 
+		  
+		  
+		  
+		  
+		 
+		 return null;
+		 
+		 
+	 }
+	 
+	 
+	
+	 
+	 
+	
+	 
+}
---
+++ Rehab/src/main/java/com/rb/entity/Thrapist.java
@@ -0,0 +1,16 @@
+package com.rb.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Thrapist {
+
+	private Integer id;
+	private String name;
+	private String email;
+	private Long mobileNumber;
+	private String gender;
+	private Map<String,String>  address= new HashMap<>();
+		 
+}
